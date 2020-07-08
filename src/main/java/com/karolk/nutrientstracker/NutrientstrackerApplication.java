d34e62a422f4cf2daf867e0f93cffce6518332aa
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-08 14:42:31 +0100

    Fixed initial project structure

---
+++ src/main/java/com/karolk/nutrientstracker/NutrientstrackerApplication.java
@@ -0,0 +1,13 @@
+package com.karolk.nutrientstracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutrientstrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutrientstrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/karolk/nutrientstracker/NutrientstrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karolk.nutrientstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NutrientstrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
