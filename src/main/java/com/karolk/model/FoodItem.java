0cdcead2c377c36a44526940864cd12a77367f19
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-10 21:31:02 +0100

    created field variables for FoodItem

--- src/main/java/com/karolk/model/FoodItem.java
+++ src/main/java/com/karolk/model/FoodItem.java
@@ -1,4 +1,15 @@
 package com.karolk.model;
 
+import java.util.Date;
+
 public class FoodItem {
+
+    private long id;
+    private String name;
+    private float amount;
+    private Date date;
+    private boolean isBreakfast;
+    private boolean isLunch;
+    private boolean isDinner;
+    private boolean isSnack;
 }
