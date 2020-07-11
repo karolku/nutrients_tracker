614ef87a3129310ad0a85c224888117442204952
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-11 17:00:07 +0100

    Modified FoodItem to follow JavaBeans assumptions.

--- src/main/java/com/karolk/model/FoodItem.java
+++ src/main/java/com/karolk/model/FoodItem.java
@@ -17,4 +17,80 @@ public class FoodItem implements Serializable {
     private boolean isDinner;
     private boolean isSnack;
 
+    public FoodItem(){}
+
+    public FoodItem(String name, float amount, Date date, boolean isBreakfast, boolean isLunch, boolean isDinner, boolean isSnack){
+        this.name = name;
+        this.amount = amount;
+        this.date = date;
+        this.isBreakfast = isBreakfast;
+        this.isLunch = isLunch;
+        this.isDinner = isDinner;
+        this.isSnack = isSnack;
+    }
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public float getAmount() {
+        return amount;
+    }
+
+    public void setAmount(float amount) {
+        this.amount = amount;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public boolean isBreakfast() {
+        return isBreakfast;
+    }
+
+    public void setBreakfast(boolean breakfast) {
+        isBreakfast = breakfast;
+    }
+
+    public boolean isLunch() {
+        return isLunch;
+    }
+
+    public void setLunch(boolean lunch) {
+        isLunch = lunch;
+    }
+
+    public boolean isDinner() {
+        return isDinner;
+    }
+
+    public void setDinner(boolean dinner) {
+        isDinner = dinner;
+    }
+
+    public boolean isSnack() {
+        return isSnack;
+    }
+
+    public void setSnack(boolean snack) {
+        isSnack = snack;
+    }
 }
