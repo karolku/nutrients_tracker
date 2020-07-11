d407f7fc5ab85afb1dfa553f87a394ca66306886
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-11 17:50:09 +0100

    added mapping method for the home controller.

--- src/main/java/com/karolk/controller/web/HomeController.java
+++ src/main/java/com/karolk/controller/web/HomeController.java
@@ -1,4 +1,16 @@
 package com.karolk.controller.web;
 
+import com.karolk.model.FoodItem;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class HomeController {
+
+    @GetMapping("/")
+    public String home(Model model){
+        model.addAttribute("foodItemModel", new FoodItem());
+        return "index";
+    }
 }
