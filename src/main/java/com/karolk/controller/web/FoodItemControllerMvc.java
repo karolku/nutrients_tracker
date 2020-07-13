1ab66b9002239c2df3e091aaf2887ff7395a905f
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-13 18:46:13 +0100

    added field variable needed for dependency injection that allow basic database operations through the implementation of the JPA which here is Hibernate.

--- src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
+++ src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
@@ -7,4 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/fooditems")
 public class FoodItemControllerMvc {
+
+    private FoodItemRepository foodItemRepository;
+
 }
