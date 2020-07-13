3ccc955024fddf670925450db82b96854faba569
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-13 19:04:20 +0100

    added list with the result of querying all of the food items in the repository.

--- src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
+++ src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
@@ -1,11 +1,14 @@
 package com.karolk.controller.web;
 
+import com.karolk.model.FoodItem;
 import com.karolk.repository.FoodItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/fooditems")
@@ -20,6 +23,7 @@ public class FoodItemControllerMvc {
 
     @GetMapping
     public String listFoodItems(Model model){
+        List<FoodItem> fooditems = foodItemRepository.findAll();
         return "foodlist";
     }
 
