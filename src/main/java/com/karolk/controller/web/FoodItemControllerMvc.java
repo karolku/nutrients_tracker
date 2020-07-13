d4e5e1234442e6856cb7e8370b20684586db2d76
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-13 18:58:01 +0100

    added method for listing all added food items with annotation.

--- src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
+++ src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
@@ -3,6 +3,8 @@ package com.karolk.controller.web;
 import com.karolk.repository.FoodItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,5 +16,9 @@ public class FoodItemControllerMvc {
     @Autowired
     public FoodItemControllerMvc(FoodItemRepository foodItemRepository){
         this.foodItemRepository = foodItemRepository;
+    }
+
+    @GetMapping
+    public String listFoodItems(Model model){
     }
 }
