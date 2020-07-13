871ca2184a9efefe2f00be2730428a2629afe0ad
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-13 17:35:20 +0100

    added repository interface

--- src/main/java/com/karolk/repository/FoodItemRepository.java
+++ src/main/java/com/karolk/repository/FoodItemRepository.java
@@ -1,4 +1,9 @@
 package com.karolk.repository;
 
-public class FoodItemRepository {
+import com.karolk.model.FoodItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FoodItemRepository extends JpaRepository<FoodItem, Long> {
 }
