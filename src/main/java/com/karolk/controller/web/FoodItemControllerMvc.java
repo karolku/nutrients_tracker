210adbda7532968195de1fb7866bca5f91d245ee
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-13 19:24:09 +0100

    added method for adding single food item.

--- src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
+++ src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
@@ -6,7 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
@@ -28,4 +31,10 @@ public class FoodItemControllerMvc {
         return "foodlist";
     }
 
+    @PostMapping
+    public String addFoodItem(@ModelAttribute FoodItem foodItemModel, RedirectAttributes redirectAttributes){
+        foodItemRepository.save(foodItemModel);
+        redirectAttributes.addFlashAttribute("messageFoodItemAdded", "Food item has been added successfully.");
+        return "redirect:/";
+    }
 }
