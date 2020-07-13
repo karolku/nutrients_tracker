362273b7c540a8985b72143406753904d3b583cb
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-13 18:21:42 +0100

    added Controller and RequestMapping annotations for mapping into fooditems.

--- src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
+++ src/main/java/com/karolk/controller/web/FoodItemControllerMvc.java
@@ -1,4 +1,10 @@
 package com.karolk.controller.web;
 
+import com.karolk.repository.FoodItemRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/fooditems")
 public class FoodItemControllerMvc {
 }
