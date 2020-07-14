5f45128250ddc399c338860309a37bab4328a343
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-14 21:31:13 +0100

    added mapping for the rest controler to display all food items.

--- src/main/java/com/karolk/controller/rest/FoodItemControllerRest.java
+++ src/main/java/com/karolk/controller/rest/FoodItemControllerRest.java
@@ -1,9 +1,17 @@
 package com.karolk.controller.rest;
 
+import com.karolk.model.FoodItem;
 import com.karolk.repository.FoodItemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/fooditems")
@@ -14,5 +22,11 @@ public class FoodItemControllerRest {
     @Autowired
     public FoodItemControllerRest(FoodItemRepository foodItemRepository){
         this.foodItemRepository = foodItemRepository;
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<FoodItem> getFoodItems(@RequestParam(defaultValue = "name") String orderBy){
+        List<FoodItem> foodItems = foodItemRepository.findAll();
+        return foodItems;
     }
 }
