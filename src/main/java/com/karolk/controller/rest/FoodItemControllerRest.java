943a0b9d23184194339b20dc03d58df4e8ac064d
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-14 20:57:33 +0100

    added rest annotations and injected the repository thorugh the constructor.

--- src/main/java/com/karolk/controller/rest/FoodItemControllerRest.java
+++ src/main/java/com/karolk/controller/rest/FoodItemControllerRest.java
@@ -1,4 +1,18 @@
 package com.karolk.controller.rest;
 
+import com.karolk.repository.FoodItemRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/fooditems")
 public class FoodItemControllerRest {
+
+    private FoodItemRepository foodItemRepository;
+
+    @Autowired
+    public FoodItemControllerRest(FoodItemRepository foodItemRepository){
+        this.foodItemRepository = foodItemRepository;
+    }
 }
