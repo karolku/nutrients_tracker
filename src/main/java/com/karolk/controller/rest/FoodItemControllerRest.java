8662ecf003aa8d32dc4e6fb9fb44cb4f499341dc
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-14 22:41:59 +0100

    added sorting for the json objects returned in the api.

--- src/main/java/com/karolk/controller/rest/FoodItemControllerRest.java
+++ src/main/java/com/karolk/controller/rest/FoodItemControllerRest.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Comparator;
 import java.util.List;
 
 @RestController
@@ -25,6 +26,11 @@ public class FoodItemControllerRest {
     @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
     public List<FoodItem> getFoodItems(@RequestParam(defaultValue = "name") String orderBy){
         List<FoodItem> foodItems = foodItemRepository.findAll();
+        if("name".equals(orderBy)){
+            foodItems.sort(Comparator.comparing(FoodItem::getName));
+        } else if("amount".equals(orderBy)){
+            foodItems.sort(Comparator.comparing(FoodItem::getAmount));
+        }
         return foodItems;
     }
 }
