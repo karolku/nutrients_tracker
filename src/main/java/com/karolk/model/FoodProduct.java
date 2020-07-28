04543d474548a970ce58f81d9a561935534c4cf1
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-07-28 22:40:34 +0100

    Created model for FoodProduct that will be provided by the external api.

---
+++ src/main/java/com/karolk/model/FoodProduct.java
@@ -0,0 +1,62 @@
+package com.karolk.model;
+
+public class FoodProduct {
+
+    private FoodSearchCriteria foodSearchCriteria;
+    private TotalHits totalHits;
+    private int currentPage;
+    private int totalPages;
+    private List<Foods> foodsList;
+
+    public FoodProduct() {
+    }
+
+    public FoodProduct(FoodSearchCriteria foodSearchCriteria, TotalHits totalHits,
+                       int currentPage, int totalPages, List<Foods> foodsList) {
+        this.foodSearchCriteria = foodSearchCriteria;
+        this.totalHits = totalHits;
+        this.currentPage = currentPage;
+        this.totalPages = totalPages;
+        this.foodsList = foodsList;
+    }
+
+    public FoodSearchCriteria getFoodSearchCriteria() {
+        return foodSearchCriteria;
+    }
+
+    public void setFoodSearchCriteria(FoodSearchCriteria foodSearchCriteria) {
+        this.foodSearchCriteria = foodSearchCriteria;
+    }
+
+    public TotalHits getTotalHits() {
+        return totalHits;
+    }
+
+    public void setTotalHits(TotalHits totalHits) {
+        this.totalHits = totalHits;
+    }
+
+    public int getCurrentPage() {
+        return currentPage;
+    }
+
+    public void setCurrentPage(int currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    public int getTotalPages() {
+        return totalPages;
+    }
+
+    public void setTotalPages(int totalPages) {
+        this.totalPages = totalPages;
+    }
+
+    public List<Foods> getFoodsList() {
+        return foodsList;
+    }
+
+    public void setFoodsList(List<Foods> foodsList) {
+        this.foodsList = foodsList;
+    }
+}
