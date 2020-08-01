a9dde7857b25ae16e816b82cbc9241f1f385a98e
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-01 22:06:09 +0100

    added constructor, getters and setters to FoodProductDto.

--- src/main/java/com/karolk/dto/FoodProductDto.java
+++ src/main/java/com/karolk/dto/FoodProductDto.java
@@ -6,4 +6,16 @@ import java.util.List;
 
 public class FoodProductDto {
     private List<Foods> foodsList;
+
+    public FoodProductDto(List<Foods> foodsList) {
+        this.foodsList = foodsList;
+    }
+
+    public List<Foods> getFoodsList() {
+        return foodsList;
+    }
+
+    public void setFoodsList(List<Foods> foodsList) {
+        this.foodsList = foodsList;
+    }
 }
