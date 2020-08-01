e4807269719f10c3cf89a7be8910b530bc0a7ea4
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-01 22:27:26 +0100

    added constructor with no parameters.

--- src/main/java/com/karolk/dto/FoodsDto.java
+++ src/main/java/com/karolk/dto/FoodsDto.java
@@ -1,11 +1,14 @@
 package com.karolk.dto;
 
+import com.karolk.model.FoodItem;
 import com.karolk.model.FoodNutrients;
 
 import java.util.List;
 
 public class FoodsDto {
     private List<FoodNutrients> foodNutrients;
+
+    public FoodsDto(){}
 
     public FoodsDto(List<FoodNutrients> foodNutrients) {
         this.foodNutrients = foodNutrients;
