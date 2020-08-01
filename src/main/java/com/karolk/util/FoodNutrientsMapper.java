46a737ef65764085067dfa20a648f2e2ab4a6abc
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-01 23:04:19 +0100

    added method to create FoodNutrientsDto object from the entity.

--- src/main/java/com/karolk/util/FoodNutrientsMapper.java
+++ src/main/java/com/karolk/util/FoodNutrientsMapper.java
@@ -1,7 +1,11 @@
 package com.karolk.util;
 
+import com.karolk.dto.FoodNutrientsDto;
+import com.karolk.model.FoodNutrients;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface FoodNutrientsMapper {
+
+    FoodNutrientsDto foodNutrientsToDto(FoodNutrients foodNutrients);
 }
