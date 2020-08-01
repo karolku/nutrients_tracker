d9fdb41713b230dbadd89ee4e4b2ddaec2bc256e
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-01 23:01:16 +0100

    added fields variables to the FoodNutrientsDto.

--- src/main/java/com/karolk/dto/FoodNutrientsDto.java
+++ src/main/java/com/karolk/dto/FoodNutrientsDto.java
@@ -1,4 +1,8 @@
 package com.karolk.dto;
 
 public class FoodNutrientsDto {
+    private Long nutrientId;
+    private String nutrientName;
+    private String unitName;
+    private double value;
 }
