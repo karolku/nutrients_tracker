be826c4721941ac8dd6177bdb1124a311630b67f
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-02 19:39:12 +0100

    added entity annotation for User

--- src/main/java/com/karolk/model/User.java
+++ src/main/java/com/karolk/model/User.java
@@ -1,5 +1,8 @@
 package com.karolk.model;
 
+import javax.persistence.Entity;
+
+@Entity
 public class User {
     private Long id;
     private String firstName;
--- src/main/java/com/karolk/repository/FoodsRepository.java
+++ src/main/java/com/karolk/repository/FoodsRepository.java
@@ -5,3 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface FoodsRepository extends JpaRepository<Foods, Long> {
 }
+
