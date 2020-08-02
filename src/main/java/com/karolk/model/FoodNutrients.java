980050140e0bad7272d04ab89c8613e1bcd5fa4b
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-02 19:02:16 +0100

    changed name of the table.

--- src/main/java/com/karolk/model/FoodNutrients.java
+++ src/main/java/com/karolk/model/FoodNutrients.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 import java.util.Objects;
 
 @Entity
-@Table(name = "food_nutrients")
+@Table(name = "nutrients")
 public class FoodNutrients implements Serializable {
 
     private static final long serialVersionUID = 42L;
