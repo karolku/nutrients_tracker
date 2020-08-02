ea1231317c4fb8f826191189deea786455ffa7ba
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-02 16:51:44 +0100

    added jpa repository for Foods

---
+++ src/main/java/com/karolk/repository/FoodsRepository.java
@@ -0,0 +1,7 @@
+package com.karolk.repository;
+
+import com.karolk.model.Foods;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FoodsRepository extends JpaRepository<Foods, Long> {
+}
