11d70e5a470a24fa3fa13ad618c4acd6f6184715
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-02 23:11:29 +0100

    added column names, hashcode and equals methods

--- src/main/java/com/karolk/model/FoodEntry.java
+++ src/main/java/com/karolk/model/FoodEntry.java
@@ -2,6 +2,7 @@ package com.karolk.model;
 
 import javax.persistence.*;
 import java.sql.Timestamp;
+import java.util.Objects;
 
 @Entity
 @Table(name = "food_entry")
@@ -10,11 +11,22 @@ public class FoodEntry {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "user_id")
     private User userId;
+
     private Foods foods;
+
+    @Column(name = "amount_of_serving")
     private double amountOfServing; // all data will be provided per 100 grams. so amount of 1 = 100 grams.
+
+    @Column(name = "serving_type")
     private String servingType;
+
+    @Column(name = "time_stamp")
     private Timestamp timestamp;
+
+    @Column(name = "meal_time")
     private String mealTime;
 
     public FoodEntry() {
@@ -82,6 +94,25 @@ public class FoodEntry {
 
     public void setServingType(String servingType) {
         this.servingType = servingType;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FoodEntry foodEntry = (FoodEntry) o;
+        return Double.compare(foodEntry.amountOfServing, amountOfServing) == 0 &&
+                Objects.equals(id, foodEntry.id) &&
+                Objects.equals(userId, foodEntry.userId) &&
+                Objects.equals(foods, foodEntry.foods) &&
+                Objects.equals(servingType, foodEntry.servingType) &&
+                Objects.equals(timestamp, foodEntry.timestamp) &&
+                Objects.equals(mealTime, foodEntry.mealTime);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, userId, foods, amountOfServing, servingType, timestamp, mealTime);
     }
 
     @Override
