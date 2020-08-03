80099cce43e8f788964fe4e92f6ebc29946e56af
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-03 15:41:00 +0100

    added method for converting FoodEntry to Dto

--- src/main/java/com/karolk/util/FoodEntryMapper.java
+++ src/main/java/com/karolk/util/FoodEntryMapper.java
@@ -1,7 +1,11 @@
 package com.karolk.util;
 
+import com.karolk.dto.FoodEntryDto;
+import com.karolk.model.FoodEntry;
 import org.mapstruct.Mapper;
 
 @Mapper
 public interface FoodEntryMapper {
+
+    FoodEntryDto foodEntryToDto(FoodEntry foodEntry);
 }
