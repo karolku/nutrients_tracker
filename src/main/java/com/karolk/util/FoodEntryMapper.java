ea67c02c88457e9a35a671ead162caabc57aeda5
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-03 15:42:33 +0100

    added instance field for FoodEntryMapper

--- src/main/java/com/karolk/util/FoodEntryMapper.java
+++ src/main/java/com/karolk/util/FoodEntryMapper.java
@@ -3,9 +3,12 @@ package com.karolk.util;
 import com.karolk.dto.FoodEntryDto;
 import com.karolk.model.FoodEntry;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface FoodEntryMapper {
+
+    FoodEntryMapper INSTANCE = Mappers.getMapper(FoodEntryMapper.class);
 
     FoodEntryDto foodEntryToDto(FoodEntry foodEntry);
 }
