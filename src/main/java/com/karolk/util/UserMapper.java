1ad14e0cfa277b0aaec07689ebce7e4492521bc2
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-03 21:17:43 +0100

    added instance of UserMapper

--- src/main/java/com/karolk/util/UserMapper.java
+++ src/main/java/com/karolk/util/UserMapper.java
@@ -1,8 +1,14 @@
 package com.karolk.util;
 
 import com.karolk.dto.UserDto;
+import com.karolk.model.User;
 import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
 
 @Mapper
 public interface UserMapper {
+
+    UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
+
+    UserDto convertUserToUserDto(User user);
 }
