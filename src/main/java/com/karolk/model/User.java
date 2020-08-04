a0d1344a64498a2858d8c9967901e0f5329f708c
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-04 17:34:42 +0100

    deleted redundant imports

--- src/main/java/com/karolk/model/User.java
+++ src/main/java/com/karolk/model/User.java
@@ -1,11 +1,7 @@
 package com.karolk.model;
 
-import org.hibernate.annotations.CreationTimestamp;
-import org.hibernate.annotations.UpdateTimestamp;
-
 import javax.persistence.*;
 import java.sql.Date;
-import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
