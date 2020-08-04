37772d1ff921ad2de66d72489eacc1fea4f51119
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-04 16:09:00 +0100

    added annotation for mapping this endpoint into api/users

--- src/main/java/com/karolk/controller/rest/UserControllerRest.java
+++ src/main/java/com/karolk/controller/rest/UserControllerRest.java
@@ -1,7 +1,9 @@
 package com.karolk.controller.rest;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("api/users")
 public class UserControllerRest {
 }
