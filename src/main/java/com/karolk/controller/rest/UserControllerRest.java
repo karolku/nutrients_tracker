5437ff4db90c8acfb5cf6028bad269165a2ffe97
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-04 19:42:25 +0100

    added save method to user controller with some additional validation to check whether a user does already exist in the db

--- src/main/java/com/karolk/controller/rest/UserControllerRest.java
+++ src/main/java/com/karolk/controller/rest/UserControllerRest.java
@@ -2,10 +2,13 @@ package com.karolk.controller.rest;
 
 import com.karolk.dto.UserDto;
 import com.karolk.service.UserService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -20,5 +23,15 @@ public class UserControllerRest {
     @GetMapping()
     List<UserDto> findAllUsers(){
         return userService.findAllUsers();
+    }
+
+    @PostMapping()
+    public ResponseEntity<UserDto> save(@RequestBody UserDto userDto) {
+        if(userDto.getId() != null)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"The object cannot have already set id.");
+        UserDto userSaved = userService.save(userDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userSaved.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(userSaved);
     }
 }
