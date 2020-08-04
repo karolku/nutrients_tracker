2a03a5078cf45ee53276de52d94c7d7de64ffae8
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-04 15:14:36 +0100

    injected UserRepository to UserService with the use of constructor

--- src/main/java/com/karolk/service/UserService.java
+++ src/main/java/com/karolk/service/UserService.java
@@ -1,7 +1,13 @@
 package com.karolk.service;
 
+import com.karolk.repository.UserRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    private UserRepository userRepository;
+
+    UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 }
