1eea324de9210fd3d1f3694bfc65e066a528a578
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-04 15:44:36 +0100

    added componentModel annotation to UserMapper

--- src/main/java/com/karolk/service/UserService.java
+++ src/main/java/com/karolk/service/UserService.java
@@ -1,13 +1,27 @@
 package com.karolk.service;
 
+import com.karolk.model.User;
 import com.karolk.repository.UserRepository;
+import com.karolk.util.UserMapper;
+import com.karolk.util.UserMapperImpl;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService {
     private UserRepository userRepository;
 
+
     UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public List<User> findAllUser(){
+        return userRepository.findAll()
+                .stream()
+                .map(UserMapper.INSTANCE.convertUserToUserDto())
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/karolk/util/UserMapper.java
+++ src/main/java/com/karolk/util/UserMapper.java
@@ -5,7 +5,7 @@ import com.karolk.model.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.factory.Mappers;
 
-@Mapper
+@Mapper(componentModel = "spring")
 public interface UserMapper {
 
     UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
