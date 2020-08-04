e1a26685f9862eb1fbddc73214f571e30ab565ee
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-04 20:49:30 +0100

    injected FoodService in the constructor and added REST annotations

--- src/main/java/com/karolk/controller/rest/FoodsControllerRest.java
+++ src/main/java/com/karolk/controller/rest/FoodsControllerRest.java
@@ -1,4 +1,15 @@
 package com.karolk.controller.rest;
 
+import com.karolk.service.FoodsService;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/foods")
 public class FoodsControllerRest {
+    private FoodsService foodsService;
+
+    public FoodsControllerRest(FoodsService foodsService) {
+        this.foodsService = foodsService;
+    }
 }
