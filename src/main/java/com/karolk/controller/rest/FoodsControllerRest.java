6e6b32550adace7c8581ab72e573157c80098c23
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-04 21:10:44 +0100

    added method to save Foods in the FoodsController

--- src/main/java/com/karolk/controller/rest/FoodsControllerRest.java
+++ src/main/java/com/karolk/controller/rest/FoodsControllerRest.java
@@ -1,10 +1,18 @@
 package com.karolk.controller.rest;
 
 import com.karolk.dto.FoodsDto;
+import com.karolk.model.Foods;
 import com.karolk.service.FoodsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @RestController
@@ -18,5 +26,15 @@ public class FoodsControllerRest {
 
     public List<FoodsDto> findAllFoods() {
         return foodsService.findAllFoods();
+    }
+
+    @PostMapping()
+    public ResponseEntity<FoodsDto> save(@RequestBody FoodsDto foodsDto) {
+        if(foodsDto.getId() != null)
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "The object cannot have already set id.");
+        FoodsDto foodsSaved = foodsService.save(foodsDto);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(foodsDto.getId())
+                .toUri();
+        return ResponseEntity.created(uri).body(foodsSaved);
     }
 }
