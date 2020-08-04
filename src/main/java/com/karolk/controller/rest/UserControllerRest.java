4e3f1a00f84f22b57597491232d4e0bd88ee5a04
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-04 16:10:27 +0100

    injected UserService with the use of constructor

--- src/main/java/com/karolk/controller/rest/UserControllerRest.java
+++ src/main/java/com/karolk/controller/rest/UserControllerRest.java
@@ -1,9 +1,15 @@
 package com.karolk.controller.rest;
 
+import com.karolk.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/users")
 public class UserControllerRest {
+    private UserService userService;
+
+    public UserControllerRest(UserService userService){
+        this.userService = userService;
+    }
 }
