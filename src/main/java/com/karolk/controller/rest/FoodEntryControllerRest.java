7a7a6a2a4ed378f585c8c7c24e1379c72586fd41
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-05 13:03:40 +0100

    added method in controller for finding all FoodEntries

--- src/main/java/com/karolk/controller/rest/FoodEntryControllerRest.java
+++ src/main/java/com/karolk/controller/rest/FoodEntryControllerRest.java
@@ -1,8 +1,12 @@
 package com.karolk.controller.rest;
 
+import com.karolk.dto.FoodEntryDto;
 import com.karolk.service.FoodEntryService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/foodEntry")
@@ -11,5 +15,10 @@ public class FoodEntryControllerRest {
 
     public FoodEntryControllerRest(FoodEntryService foodEntryService) {
         this.foodEntryService = foodEntryService;
+    }
+
+    @GetMapping()
+    public List<FoodEntryDto> findAllFoodEntries() {
+        return foodEntryService.findAllFoodEntries();
     }
 }
