88abcdd0224c69a594ded3a22ebf1b046815b6da
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-05 12:45:36 +0100

    Injected foodEntryRepositroy through constructor of the service

--- src/main/java/com/karolk/service/FoodEntryService.java
+++ src/main/java/com/karolk/service/FoodEntryService.java
@@ -1,4 +1,13 @@
 package com.karolk.service;
 
+import com.karolk.repository.FoodEntryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class FoodEntryService {
+    FoodEntryRepository foodEntryRepository;
+
+    public FoodEntryService(FoodEntryRepository foodEntryRepository) {
+        this.foodEntryRepository = foodEntryRepository;
+    }
 }
