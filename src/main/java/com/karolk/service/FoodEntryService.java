e5e750f8131ea006f829c30148a5fba114732627
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-05 12:49:43 +0100

    added method that finds and returns all FoodEntries

--- src/main/java/com/karolk/service/FoodEntryService.java
+++ src/main/java/com/karolk/service/FoodEntryService.java
@@ -1,7 +1,13 @@
 package com.karolk.service;
 
+import com.karolk.dto.FoodEntryDto;
+import com.karolk.model.FoodEntry;
 import com.karolk.repository.FoodEntryRepository;
+import com.karolk.util.FoodEntryMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class FoodEntryService {
@@ -9,5 +15,12 @@ public class FoodEntryService {
 
     public FoodEntryService(FoodEntryRepository foodEntryRepository) {
         this.foodEntryRepository = foodEntryRepository;
+    }
+
+    public List<FoodEntryDto> findAllFoodEntries() {
+        return foodEntryRepository.findAll()
+                .stream()
+                .map(FoodEntryMapper.INSTANCE::convertFoodEntryToDto)
+                .collect(Collectors.toList());
     }
 }
