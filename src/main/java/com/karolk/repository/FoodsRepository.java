c09023750a2c81a68851b2ae3ec982dde6dcbc7c
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-05 15:53:39 +0100

    added query to find foods by fdcId in FoodsRepository.

--- src/main/java/com/karolk/repository/FoodsRepository.java
+++ src/main/java/com/karolk/repository/FoodsRepository.java
@@ -3,6 +3,9 @@ package com.karolk.repository;
 import com.karolk.model.Foods;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface FoodsRepository extends JpaRepository<Foods, Long> {
+    Optional<Foods> findByFdcId(Long fdcId);
 }
 
