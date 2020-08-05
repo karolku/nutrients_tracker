98f64f9d020c5698671af83b1aae492a19c35ee2
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-05 13:01:32 +0100

    injected FoodEntryService to controller's constructor

--- src/main/java/com/karolk/controller/rest/FoodEntryControllerRest.java
+++ src/main/java/com/karolk/controller/rest/FoodEntryControllerRest.java
@@ -1,9 +1,15 @@
 package com.karolk.controller.rest;
 
+import com.karolk.service.FoodEntryService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/foodEntry")
 public class FoodEntryControllerRest {
+    private FoodEntryService foodEntryService;
+
+    public FoodEntryControllerRest(FoodEntryService foodEntryService) {
+        this.foodEntryService = foodEntryService;
+    }
 }
