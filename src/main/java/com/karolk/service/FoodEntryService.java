e72a7b3c5f9f01c3afeb15185d76cbda18dfec64
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-05 15:41:15 +0100

    injected userRepository and foodsRepository into the constructor

--- src/main/java/com/karolk/service/FoodEntryService.java
+++ src/main/java/com/karolk/service/FoodEntryService.java
@@ -3,6 +3,8 @@ package com.karolk.service;
 import com.karolk.dto.FoodEntryDto;
 import com.karolk.model.FoodEntry;
 import com.karolk.repository.FoodEntryRepository;
+import com.karolk.repository.FoodsRepository;
+import com.karolk.repository.UserRepository;
 import com.karolk.util.FoodEntryMapper;
 import org.springframework.stereotype.Service;
 
@@ -12,9 +14,15 @@ import java.util.stream.Collectors;
 @Service
 public class FoodEntryService {
     FoodEntryRepository foodEntryRepository;
+    UserRepository userRepository;
+    FoodsRepository foodsRepository;
 
-    public FoodEntryService(FoodEntryRepository foodEntryRepository) {
+    public FoodEntryService(FoodEntryRepository foodEntryRepository,
+                            UserRepository userRepository,
+                            FoodsRepository foodsRepository) {
         this.foodEntryRepository = foodEntryRepository;
+        this.userRepository = userRepository;
+        this.foodsRepository = foodsRepository;
     }
 
     public List<FoodEntryDto> findAllFoodEntries() {
