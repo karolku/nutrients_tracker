96229eaeff679d25c2d8b9e6859b79c5e363363b
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-05 16:39:09 +0100

    changed type and name of the timestamp field variable

--- src/main/java/com/karolk/dto/FoodEntryDto.java
+++ src/main/java/com/karolk/dto/FoodEntryDto.java
@@ -4,6 +4,7 @@ import com.karolk.model.FoodEntry;
 import com.karolk.model.Foods;
 import com.karolk.model.User;
 
+import java.sql.Date;
 import java.sql.Timestamp;
 import java.util.Objects;
 
@@ -14,18 +15,19 @@ public class FoodEntryDto {
     private Long fdcId;
     private double amountOfServing;
     private String servingType;
-    private Timestamp timestamp;
+    private Date dateOfFoodEntry;
     private FoodEntry.MealTime mealTime;
 
     public FoodEntryDto() {
     }
 
-    public FoodEntryDto(Long user_id, Long fdcId, double amountOfServing, String servingType, Timestamp timestamp, FoodEntry.MealTime mealTime) {
+    public FoodEntryDto(Long user_id, Long fdcId, double amountOfServing, String servingType,
+                        Date dateOfFoodEntry, FoodEntry.MealTime mealTime) {
         this.user_id = user_id;
         this.fdcId = fdcId;
         this.amountOfServing = amountOfServing;
         this.servingType = servingType;
-        this.timestamp = timestamp;
+        this.dateOfFoodEntry = dateOfFoodEntry;
         this.mealTime = mealTime;
     }
 
@@ -69,12 +71,12 @@ public class FoodEntryDto {
         this.servingType = servingType;
     }
 
-    public Timestamp getTimestamp() {
-        return timestamp;
+    public Date getDateOfFoodEntry() {
+        return dateOfFoodEntry;
     }
 
-    public void setTimestamp(Timestamp timestamp) {
-        this.timestamp = timestamp;
+    public void setDateOfFoodEntry(Date dateOfFoodEntry) {
+        this.dateOfFoodEntry = dateOfFoodEntry;
     }
 
     public FoodEntry.MealTime getMealTime() {
@@ -95,13 +97,13 @@ public class FoodEntryDto {
                 Objects.equals(user_id, that.user_id) &&
                 Objects.equals(fdcId, that.fdcId) &&
                 Objects.equals(servingType, that.servingType) &&
-                Objects.equals(timestamp, that.timestamp) &&
+                Objects.equals(dateOfFoodEntry, that.dateOfFoodEntry) &&
                 mealTime == that.mealTime;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(id, user_id, fdcId, amountOfServing, servingType, timestamp, mealTime);
+        return Objects.hash(id, user_id, fdcId, amountOfServing, servingType, dateOfFoodEntry, mealTime);
     }
 
     @Override
@@ -112,7 +114,7 @@ public class FoodEntryDto {
                 ", fdcId=" + fdcId +
                 ", amountOfServing=" + amountOfServing +
                 ", servingType='" + servingType + '\'' +
-                ", timestamp=" + timestamp +
+                ", timestamp=" + dateOfFoodEntry +
                 ", mealTime=" + mealTime +
                 '}';
     }
