331e18a014bf3340d074e7e3acd984be1b8c0384
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-06 20:53:18 +0100

    injected repositories inside the constructor

--- src/main/java/com/karolk/service/FoodsNutrientsService.java
+++ src/main/java/com/karolk/service/FoodsNutrientsService.java
@@ -1,7 +1,21 @@
 package com.karolk.service;
 
+import com.karolk.repository.FoodsNutrientsRepository;
+import com.karolk.repository.FoodsRepository;
+import com.karolk.repository.NutrientsRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class FoodsNutrientsService {
+    private FoodsRepository foodsRepository;
+    private NutrientsRepository nutrientsRepository;
+    private FoodsNutrientsRepository foodsNutrientsRepository;
+
+    public FoodsNutrientsService(FoodsRepository foodsRepository,
+                                 NutrientsRepository nutrientsRepository,
+                                 FoodsNutrientsRepository foodsNutrientsRepository) {
+        this.foodsRepository = foodsRepository;
+        this.nutrientsRepository = nutrientsRepository;
+        this.foodsNutrientsRepository = foodsNutrientsRepository;
+    }
 }
