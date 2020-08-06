5aa28968687fd986ff1b636f4ee6edfed9648236
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-06 21:59:29 +0100

    added method to FoodProducdt rest to get data about Foods from usda

--- src/main/java/com/karolk/controller/rest/FoodProductControllerRest.java
+++ src/main/java/com/karolk/controller/rest/FoodProductControllerRest.java
@@ -1,4 +1,26 @@
 package com.karolk.controller.rest;
 
+import com.karolk.model.Foods;
+import com.karolk.service.GetFoodProductModelService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/usda")
 public class FoodProductControllerRest {
+    private GetFoodProductModelService getFoodProductModelService;
+
+    public FoodProductControllerRest(GetFoodProductModelService getFoodService){
+        this.getFoodProductModelService = getFoodService;
+    }
+
+    @GetMapping(path = "/foods/{fdcId}")
+    public List<Foods> getFoodsInfoFromApi(@PathVariable("fdcId") Long fdcId){
+        List<Foods> foodsList = getFoodProductModelService.getFoodsInfoFromApi(fdcId);
+        return foodsList;
+    }
 }
