38b8e3f2892a609e3c27ac716fb4b100faae3f29
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-06 21:01:55 +0100

    added methods for converting FoodNutrients entity to Dto

--- src/main/java/com/karolk/util/FoodsNutrientsMapper.java
+++ src/main/java/com/karolk/util/FoodsNutrientsMapper.java
@@ -1,5 +1,24 @@
 package com.karolk.util;
 
+import com.karolk.dto.FoodsNutrientsDto;
+import com.karolk.model.Foods;
+import com.karolk.model.FoodsNutrients;
+import com.karolk.model.Nutrients;
+import org.mapstruct.Mapper;
+import org.mapstruct.factory.Mappers;
+
+@Mapper
 public interface FoodsNutrientsMapper {
+    FoodsNutrientsMapper INSTANCE = Mappers.getMapper(FoodsNutrientsMapper.class);
+
+    FoodsNutrientsDto convertEntityToDto(FoodsNutrients foodsNutrients);
+
+    default Long toLongFoods(Foods foods) {
+        return foods.getId();
+    }
+
+    default Long toLongNutrients(Nutrients nutrients) {
+        return nutrients.getId();
+    }
 }
 
