6eab2ad471890b69b2977d5141cd8c66d1ab9fa4
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-06 14:12:09 +0100

    added custom query to find FoodEntry by userId in FoodEntry Repository

--- src/main/java/com/karolk/repository/FoodEntryRepository.java
+++ src/main/java/com/karolk/repository/FoodEntryRepository.java
@@ -3,5 +3,8 @@ package com.karolk.repository;
 import com.karolk.model.FoodEntry;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FoodEntryRepository extends JpaRepository<FoodEntry, Long> {
+    List<FoodEntry> findFoodEntriesByUserId(Long userId);
 }
