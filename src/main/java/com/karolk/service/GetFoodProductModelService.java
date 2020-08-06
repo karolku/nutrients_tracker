ca46d4fa8fe3f36a8dfec8106b65ca59b52e6f7f
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-06 23:36:07 +0100

    removed api key from propertires and put the key inside the class instead, since @Value didnt load the variable from properties

--- src/main/java/com/karolk/service/GetFoodProductModelService.java
+++ src/main/java/com/karolk/service/GetFoodProductModelService.java
@@ -13,9 +13,7 @@ import java.util.List;
 @Service
 public class GetFoodProductModelService {
 
-    @Value("${api.key}")
-    private String apiKey;
-
+    private String apiKey = "CzUj0Hv2GnZ0RIyoL8iKGHuix4QLpR4Te4w3hpMv";
     private RestTemplate restTemplate;
 
     @Autowired
@@ -26,7 +24,7 @@ public class GetFoodProductModelService {
     public List<FoodsApi> getFoodsInfoFromApi(Long fdcId){
         FoodProduct foodProduct = restTemplate.
                 getForObject("https://api.nal.usda.gov/fdc/v1/foods/search?query=" +
-                        fdcId + "&dataType=Branded&pageSize=25&pageNumber=1&api_key=" + "CzUj0Hv2GnZ0RIyoL8iKGHuix4QLpR4Te4w3hpMv",
+                        fdcId + "&dataType=Branded&pageSize=25&pageNumber=1&api_key=" + apiKey,
                 FoodProduct.class);
         List<FoodsApi> foodsApiList = foodProduct.getFoods();
         return foodsApiList;
