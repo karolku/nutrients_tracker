7141a1981a3ca5bb5441bcc90770b91ead6b053b
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-06 20:46:00 +0100

    added fields variables with annotations

--- src/main/java/com/karolk/model/FoodsNutrients.java
+++ src/main/java/com/karolk/model/FoodsNutrients.java
@@ -1,4 +1,24 @@
 package com.karolk.model;
 
-public class FoodsNutrients {
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name = "foods_nutrients")
+public class FoodsNutrients implements Serializable {
+
+    private static final long serialVersionUID = 42L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "foods_nutrients_id")
+    private Long id;
+
+    @ManyToOne
+    @Column(name = "food_id")
+    private Foods foods;
+
+    @ManyToOne
+    @Column(name = "nutrient_id")
+    private Nutrients nutrients;
 }
