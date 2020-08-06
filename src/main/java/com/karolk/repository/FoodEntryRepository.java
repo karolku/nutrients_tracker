a4a0fdb05a45b5cb9f99e83e406da2f73e8d027c
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-06 15:02:50 +0100

    changed type of the input variable in the custom query

--- src/main/java/com/karolk/repository/FoodEntryRepository.java
+++ src/main/java/com/karolk/repository/FoodEntryRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface FoodEntryRepository extends JpaRepository<FoodEntry, Long> {
-    List<FoodEntry> findFoodEntriesByUserId(Long userId);
+    List<FoodEntry> findFoodEntriesByUserId(User userId);
 }
