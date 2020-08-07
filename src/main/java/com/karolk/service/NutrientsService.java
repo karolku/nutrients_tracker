7be4fc06bc88c790035b7c64c69856983839b329
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-07 22:17:56 +0100

    injected nutrients repository through constructor

--- src/main/java/com/karolk/service/NutrientsService.java
+++ src/main/java/com/karolk/service/NutrientsService.java
@@ -1,7 +1,13 @@
 package com.karolk.service;
 
+import com.karolk.repository.NutrientsRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NutrientsService {
+    private NutrientsRepository nutrientsRepository;
+
+    public NutrientsService(NutrientsRepository nutrientsRepository) {
+        this.nutrientsRepository = nutrientsRepository;
+    }
 }
