2ed26c655fc58c2e7e9530bfaa09663af6f49659
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-07 22:33:27 +0100

    added method to save nutrients. Added custom query in nutrients repository.

--- src/main/java/com/karolk/repository/NutrientsRepository.java
+++ src/main/java/com/karolk/repository/NutrientsRepository.java
@@ -4,4 +4,5 @@ import com.karolk.model.Nutrients;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface NutrientsRepository extends JpaRepository<Nutrients, Long> {
+    Nutrients findByNutrientId(Long nutrientId);
 }
--- src/main/java/com/karolk/service/NutrientsService.java
+++ src/main/java/com/karolk/service/NutrientsService.java
@@ -1,7 +1,15 @@
 package com.karolk.service;
 
+import com.karolk.api.model.NutrientsApi;
+import com.karolk.model.Nutrients;
 import com.karolk.repository.NutrientsRepository;
+import com.karolk.util.NutrientsMapper;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class NutrientsService {
@@ -9,5 +17,22 @@ public class NutrientsService {
 
     public NutrientsService(NutrientsRepository nutrientsRepository) {
         this.nutrientsRepository = nutrientsRepository;
+    }
+
+    public List<Nutrients> saveNutrients(List<NutrientsApi> nutrientsApiList) {
+        List<Nutrients> nutrientsSaved = new ArrayList<>();
+        Nutrients nutrientFromDb = null;
+        List<Nutrients> nutrientsEntityList = nutrientsApiList.stream()
+                .map(NutrientsMapper.INSTANCE::convertNutrientsApiToEntity)
+                .collect(Collectors.toList());
+        for(Nutrients nutrient : nutrientsEntityList){
+            if(nutrientsRepository.findById(nutrient.getId()).isEmpty())
+                nutrientsSaved.add(nutrientsRepository.save(nutrient));
+            else {
+                nutrientFromDb = nutrientsRepository.findByNutrientId(nutrient.getNutrientId());
+                nutrientsSaved.add(nutrientFromDb);
+            }
+        }
+        return nutrientsSaved;
     }
 }
