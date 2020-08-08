a7684fc9551d290d4619c0c5247ee64231fd066a
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-08 21:54:11 +0100

    added custom query existsByNutrientId to Nutrients repository

--- src/main/java/com/karolk/repository/NutrientsRepository.java
+++ src/main/java/com/karolk/repository/NutrientsRepository.java
@@ -7,4 +7,5 @@ import java.util.Optional;
 
 public interface NutrientsRepository extends JpaRepository<Nutrients, Long> {
     Optional<Nutrients> findByNutrientId(Long nutrientId);
+    boolean existsByNutrientId(Long nutrientId);
 }
