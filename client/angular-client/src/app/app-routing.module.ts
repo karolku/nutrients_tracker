52b2aa6b37badf683754541dacb93b34fac43104
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-16 16:00:32 +0100

    added custom routes for testing in app-routing module

--- client/angular-client/src/app/app-routing.module.ts
+++ client/angular-client/src/app/app-routing.module.ts
@@ -1,3 +1,5 @@
+import { SettingsComponent } from './settings/settings.component';
+import { MainComponent } from './main/main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -5,7 +7,10 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot([
+    {path: '', component: MainComponent},
+    {path: 'settings', component: SettingsComponent}
+  ])],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
