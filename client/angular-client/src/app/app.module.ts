9bf0d3825bd9babac50a05636955242964a8c5a7
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-16 12:07:56 +0100

    added footer, header, main, sidenav components

--- client/angular-client/src/app/app.module.ts
+++ client/angular-client/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
