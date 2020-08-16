9841403e7ffcdb74511364c7a263522bae5ebe8f
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-16 17:49:40 +0100

    changed route for home page

--- client/angular-client/src/app/app-routing.module.ts
+++ client/angular-client/src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
+import { HomeComponent } from './home/home.component';
 import { SettingsComponent } from './settings/settings.component';
-import { MainComponent } from './main/main.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -8,7 +8,7 @@ const routes: Routes = [];
 
 @NgModule({
   imports: [RouterModule.forRoot([
-    {path: '', component: MainComponent},
+    {path: '', component: HomeComponent},
     {path: 'settings', component: SettingsComponent}
   ])],
   exports: [RouterModule]
