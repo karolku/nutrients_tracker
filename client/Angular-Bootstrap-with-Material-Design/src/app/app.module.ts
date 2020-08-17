64d4bd25146e1243d8a304499b044d6b83ea5b04
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-17 12:02:55 +0100

    generated main components

--- client/Angular-Bootstrap-with-Material-Design/src/app/app.module.ts
+++ client/Angular-Bootstrap-with-Material-Design/src/app/app.module.ts
@@ -4,10 +4,22 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    DashboardComponent,
+    FooterComponent,
+    HomeComponent,
+    SettingsComponent,
+    SidenavComponent
   ],
   imports: [
     BrowserModule,
