a03f2f9d32cabf5a83e270e6859f8e9179fab98e
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-19 19:35:40 +0100

    injected fields through the constructor

--- src/main/java/com/karolk/service/AuthenticationService.java
+++ src/main/java/com/karolk/service/AuthenticationService.java
@@ -1,7 +1,17 @@
 package com.karolk.service;
 
+import com.karolk.repository.UserRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthenticationService {
+
+    private UserRepository userRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
 }
