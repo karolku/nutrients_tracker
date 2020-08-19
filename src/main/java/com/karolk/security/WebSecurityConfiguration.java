a025f5b976189fb84186b25a6ddf97698562505f
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-19 16:45:38 +0100

    changed extension of the class to WebSecurityConfigurerAdapter

--- src/main/java/com/karolk/security/WebSecurityConfiguration.java
+++ src/main/java/com/karolk/security/WebSecurityConfiguration.java
@@ -1,6 +1,6 @@
 package com.karolk.security;
 
-import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
-public class WebSecurityConfiguration extends SecurityConfigurerAdapter {
+public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 }
