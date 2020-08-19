971e95c3f9e1ab8a77fe9ad0bb816e50d830baa9
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-19 19:26:47 +0100

    added method for signup in the authentication controller

--- src/main/java/com/karolk/controller/rest/AuthenticationController.java
+++ src/main/java/com/karolk/controller/rest/AuthenticationController.java
@@ -1,5 +1,9 @@
 package com.karolk.controller.rest;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,5 +17,9 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
-
+    @PostMapping("/signup")
+    public ResponseEntity signUp(@RequestBody RegisterRequest registerRequest) {
+        authenticationService.signUp(registerRequest);
+        return new ResponseEntity(HttpStatus.OK);
+    }
 }
