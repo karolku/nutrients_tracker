49cc48fac965d5f0a6647f2d8732a033b4f36ca9
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-19 16:52:48 +0100

    added method to allow cross origin resource sharing for the angular client run under localhost:4200

--- src/main/java/com/karolk/security/WebSecurityConfiguration.java
+++ src/main/java/com/karolk/security/WebSecurityConfiguration.java
@@ -1,6 +1,33 @@
 package com.karolk.security;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+import java.util.Arrays;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors(withDefaults());
+    }
+
+    // https://docs.spring.io/spring-security/site/docs/current/reference/html5/#cors
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**",configuration);
+        return source;
+    }
 }
