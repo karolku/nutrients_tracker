db6c3a49758bc30d868ec1c60a2b61eb1d9c5c0d
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-19 20:09:28 +0100

    added signup method in authentication service

--- src/main/java/com/karolk/service/AuthenticationService.java
+++ src/main/java/com/karolk/service/AuthenticationService.java
@@ -1,5 +1,7 @@
 package com.karolk.service;
 
+import com.karolk.dto.UserDto;
+import com.karolk.model.User;
 import com.karolk.repository.UserRepository;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -13,5 +15,22 @@ public class AuthenticationService {
     public AuthenticationService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.passwordEncoder = passwordEncoder;
+    }
+
+    public void signup(UserDto userDto) {
+        User user = new User();
+        user.setFirstName(userDto.getFirstName());
+        user.setLastName(userDto.getLastName());
+        if (userDto.getGender() != null) {
+            user.setGender((User.Gender)Enum.valueOf(User.Gender.class, userDto.getGender()));
+        }
+        user.setBirthdayDate(userDto.getBirthdayDate());
+        user.setEmail(userDto.getEmail());
+        user.setPassword(passwordEncoder.encode(userDto.getPassword()));
+        user.setWeightInKg(userDto.getWeightInKg());
+        user.setHeightInCm(userDto.getHeightInCm());
+        user.setActivityLevel(userDto.getActivityLevel());
+
+        userRepository.save(user);
     }
 }
