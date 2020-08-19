8c478fc76a91245a9ef4bd57423d659456f28b0d
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-19 16:35:44 +0100

    added security configuration class

---
+++ src/main/java/com/karolk/security/WebSecurityConfiguration.java
@@ -0,0 +1,6 @@
+package com.karolk.security;
+
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
+
+public class WebSecurityConfiguration extends SecurityConfigurerAdapter {
+}
