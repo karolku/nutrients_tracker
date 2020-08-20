992d0ac30c1fcebb0b6782267af52c2725c06259
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-20 13:01:26 +0100

    fixed access modifiers

--- src/main/java/com/karolk/model/UserDetailsImpl.java
+++ src/main/java/com/karolk/model/UserDetailsImpl.java
@@ -15,36 +15,36 @@ public class UserDetailsImpl implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return grantedAuthorityList;
     }
 
     @Override
     public String getPassword() {
-        return null;
+        return password;
     }
 
     @Override
     public String getUsername() {
-        return null;
+        return email;
     }
 
     @Override
     public boolean isAccountNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isAccountNonLocked() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isCredentialsNonExpired() {
-        return false;
+        return true;
     }
 
     @Override
     public boolean isEnabled() {
-        return false;
+        return active;
     }
 }
