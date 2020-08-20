88954a6d4bea461a504afeda6026b666046317f6
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-20 13:07:11 +0100

    added constructor to user details impl

--- src/main/java/com/karolk/model/UserDetailsImpl.java
+++ src/main/java/com/karolk/model/UserDetailsImpl.java
@@ -1,8 +1,11 @@
 package com.karolk.model;
 
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -12,6 +15,13 @@ public class UserDetailsImpl implements UserDetails {
     private String password;
     private boolean active;
     private List<GrantedAuthority> grantedAuthorityList;
+
+    public UserDetailsImpl(User user) {
+        this.email = user.getEmail();
+        this.password = user.getPassword();
+        this.active = user.isActive();
+        this.grantedAuthorityList = Arrays.asList((new SimpleGrantedAuthority("USER")));
+    }
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
