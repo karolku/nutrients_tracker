c7dc1fb5314fcd57d0ed2f1403e54e7e66735d48
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-20 12:36:19 +0100

    injected user repository through constructor and overridden the loadUserByUserName method

--- src/main/java/com/karolk/model/UserDetailsServiceImpl.java
+++ src/main/java/com/karolk/model/UserDetailsServiceImpl.java
@@ -1,12 +1,23 @@
 package com.karolk.model;
 
+import com.karolk.repository.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+@Service
 public class UserDetailsServiceImpl implements UserDetailsService {
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> user = userRepository.findByEmail(email);
     }
 }
