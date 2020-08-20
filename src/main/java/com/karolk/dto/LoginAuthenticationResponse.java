ed0f1622c7ad4eca9a679a9bf473fa703d9f2911
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-20 15:57:14 +0100

    added fields and constructor to the class

--- src/main/java/com/karolk/dto/LoginAuthenticationResponse.java
+++ src/main/java/com/karolk/dto/LoginAuthenticationResponse.java
@@ -1,4 +1,20 @@
 package com.karolk.dto;
 
 public class LoginAuthenticationResponse {
+
+    private String jwt;
+    private String username;
+
+    public LoginAuthenticationResponse(String jwt, String username) {
+        this.jwt = jwt;
+        this.username = username;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+
+    public String getUsername() {
+        return username;
+    }
 }
