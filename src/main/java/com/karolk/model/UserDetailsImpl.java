730b102e6b3607121aacb895eaa7ff70fc02c47e
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-20 12:50:42 +0100

    added field variables

--- src/main/java/com/karolk/model/UserDetailsImpl.java
+++ src/main/java/com/karolk/model/UserDetailsImpl.java
@@ -4,8 +4,15 @@ import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 public class UserDetailsImpl implements UserDetails {
+
+    private String email;
+    private String password;
+    private boolean active;
+    private List<GrantedAuthority> grantedAuthorityList;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
         return null;
