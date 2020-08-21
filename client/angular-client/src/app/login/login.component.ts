a43454db93d983886805874754ad82fb8012c32c
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-21 18:13:21 +0100

    fixed import path

--- client/angular-client/src/app/login/login.component.ts
+++ client/angular-client/src/app/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AuthenticationService } from './../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthenticationService) { }
 
   ngOnInit(): void {
   }
