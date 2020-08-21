48694e522cf0e1cf7aafbafc69c7a58d3c620f06
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-21 22:03:08 +0100

    fixed issue with CORS by changing the configure method and corsConfigurationSource in WebSecurityConfig class

--- src/main/java/com/karolk/controller/rest/AuthenticationController.java
+++ src/main/java/com/karolk/controller/rest/AuthenticationController.java
@@ -7,10 +7,7 @@ import com.karolk.service.AuthenticationService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/authentication")
--- src/main/java/com/karolk/security/WebSecurityConfiguration.java
+++ src/main/java/com/karolk/security/WebSecurityConfiguration.java
@@ -19,7 +19,9 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import java.util.Arrays;
+import java.util.List;
 
+import static java.util.Collections.singletonList;
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
@@ -46,7 +48,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.cors(withDefaults());
+        http.cors().configurationSource(corsConfigurationSource());
         http.csrf().disable()
                 .authorizeRequests().antMatchers("/api/authentication/**").permitAll()
                 .anyRequest().authenticated()
@@ -60,8 +62,11 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("http://localhost:4200"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE"));
+        List<String> allowOrigins = Arrays.asList("*");
+        configuration.setAllowedOrigins(allowOrigins);
+        configuration.setAllowedMethods(singletonList("*"));
+        configuration.setAllowedHeaders(singletonList("*"));
+        configuration.setAllowCredentials(true);
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**",configuration);
         return source;
