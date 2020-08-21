a757f69459a79073b624c5f2002062b2a153f5dd
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-21 18:21:52 +0100

    Added signIn method in login component

--- client/angular-client/src/app/login/login.component.ts
+++ client/angular-client/src/app/login/login.component.ts
@@ -1,3 +1,6 @@
+import { NavbarComponent } from './../navbar/navbar.component';
+import { AppRoutingModule } from './../app-routing.module';
+import { RouterModule, Router } from '@angular/router';
 import { AuthenticationService } from './../services/authentication.service';
 import { Component, OnInit } from '@angular/core';
 
@@ -7,10 +10,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  invalidLogin: boolean;
 
-  constructor(private auth: AuthenticationService) { }
+  constructor(private auth: AuthenticationService,
+              private router: Router) { }
 
   ngOnInit(): void {
   }
 
+  signIn(credentials){
+    this.auth.login(credentials)
+      .subscribe(response => {
+        if(response)
+          this.router.navigate(['/']);
+        else
+          this.invalidLogin = true;
+      })
+  }
 }
