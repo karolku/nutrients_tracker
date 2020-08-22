ba4b288d9ab8b029ab0fce2069a207e2a6b42795
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-22 15:44:00 +0100

    Added canActivate method and injected authenticationService and Router

--- client/angular-client/src/app/services/auth-guard.service.ts
+++ client/angular-client/src/app/services/auth-guard.service.ts
@@ -1,9 +1,18 @@
+import { AuthenticationService } from './authentication.service';
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, RouterModule, Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+  constructor(private auth: AuthenticationService, private router: Router) { }
+  canActivate() {
+    if(this.auth.isLoggedIn) 
+      return true;
+    else
+      this.router.navigate(['/login']);
+      return false;
+  }
 }
