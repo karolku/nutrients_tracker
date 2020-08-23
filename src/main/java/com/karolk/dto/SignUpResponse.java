abe75b369b56e147f7fce54af916ce09c68a8a6c
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-23 21:24:00 +0100

    Created SignUpresponse class

---
+++ src/main/java/com/karolk/dto/SignUpResponse.java
@@ -0,0 +1,67 @@
+package com.karolk.dto;
+
+import java.util.Objects;
+
+public class SignUpResponse {
+
+    private Long id;
+    private String firstName;
+    private String email;
+
+    public SignUpResponse() {
+    }
+
+    public SignUpResponse(Long id, String firstName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        SignUpResponse that = (SignUpResponse) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(firstName, that.firstName) &&
+                Objects.equals(email, that.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, email);
+    }
+
+    @Override
+    public String toString() {
+        return "SignUpResponse{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
