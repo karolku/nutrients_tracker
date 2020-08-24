e43e413f487eb7490a2a6445a268a5523e56fc1f
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-24 15:22:25 +0100

    Added id field to LoginAuthResponse

--- src/main/java/com/karolk/dto/LoginAuthenticationResponse.java
+++ src/main/java/com/karolk/dto/LoginAuthenticationResponse.java
@@ -2,12 +2,14 @@ package com.karolk.dto;
 
 public class LoginAuthenticationResponse {
 
+    private Long id;
     private String jwt;
     private String username;
 
-    public LoginAuthenticationResponse(String jwt, String username) {
+    public LoginAuthenticationResponse(String jwt, String username, Long id) {
         this.jwt = jwt;
         this.username = username;
+        this.id = id;
     }
 
     public String getJwt() {
@@ -16,5 +18,13 @@ public class LoginAuthenticationResponse {
 
     public String getUsername() {
         return username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 }
