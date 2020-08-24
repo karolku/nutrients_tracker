cd741578d4d0201c6413c8e1c1d94e4d57d893c5
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-24 22:25:12 +0100

    Modified userName field in home component

--- client/angular-client/src/app/home/home.component.ts
+++ client/angular-client/src/app/home/home.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
-  userName = localStorage.getItem('firstName');
+  userName = {
+    firstName: localStorage.getItem('firstName')
+  }
 
   constructor(public auth: AuthenticationService) { }
 
