8476801f4ae37cb323707b0bc59e7cbc2a45015f
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-25 22:10:10 +0100

    Added method to foodEntry service

--- client/angular-client/src/app/services/food-entry.service.ts
+++ client/angular-client/src/app/services/food-entry.service.ts
@@ -1,10 +1,21 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FoodEntryService {
+  private url = 'http://localhost:8080/api/foodEntry';
 
   constructor(private httpClient: HttpClient) { }
+
+  createFoodEntry(entryData) {
+    let token = localStorage.getItem('jwt');
+
+    let authHeaders = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer ' + token
+  });
+    this.httpClient.post(this.url, JSON.stringify(entryData), {headers: authHeaders});
+  }
 }
