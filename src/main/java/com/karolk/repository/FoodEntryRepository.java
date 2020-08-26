3062b2b5e3bc7b1de8c23a1cf4b4e71b669e0101
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-26 13:45:05 +0100

    Added method to the foodEntry repository

--- src/main/java/com/karolk/repository/FoodEntryRepository.java
+++ src/main/java/com/karolk/repository/FoodEntryRepository.java
@@ -4,8 +4,10 @@ import com.karolk.model.FoodEntry;
 import com.karolk.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Date;
 import java.util.List;
 
 public interface FoodEntryRepository extends JpaRepository<FoodEntry, Long> {
     List<FoodEntry> findFoodEntriesByUserId(User userId);
+    List<FoodEntry> findFoodEntriesByUserIdAndDateOfFoodEntry(User userId, Date foodEntryDate);
 }
