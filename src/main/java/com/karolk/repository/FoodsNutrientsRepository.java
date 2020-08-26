596fa58cb5afce14c3711bcaa9232766e6ca620a
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-26 16:49:01 +0100

    Added query to the FoodNutrinets repository

--- src/main/java/com/karolk/repository/FoodsNutrientsRepository.java
+++ src/main/java/com/karolk/repository/FoodsNutrientsRepository.java
@@ -1,7 +1,11 @@
 package com.karolk.repository;
 
+import com.karolk.model.FoodEntry;
 import com.karolk.model.FoodsNutrients;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface FoodsNutrientsRepository extends JpaRepository<FoodsNutrients, Long> {
+    List<FoodsNutrients> findFoodsNutrientsByFoodEntry(FoodEntry foodEntry);
 }
