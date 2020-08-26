43661ed850eaafe8fa247e36cea60d62bce9ed6d
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-08-26 15:27:47 +0100

    Added subscription to the method in the dashboard component

--- client/angular-client/src/app/dashboard/dashboard.component.ts
+++ client/angular-client/src/app/dashboard/dashboard.component.ts
@@ -1,3 +1,4 @@
+import { FoodEntryService } from './../services/food-entry.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   foodEntries: any[];
 
-  constructor() { }
+  constructor(private foodEntryService: FoodEntryService) { }
 
   ngOnInit(): void {
   }
@@ -51,6 +52,10 @@ export class DashboardComponent implements OnInit {
 
 
   getFoodEntriesByDateAndUserId(date) {
-    console.log(date);
+    this.foodEntryService.getFoodEntryByUserIdAndDate(date)
+      .subscribe((response: any) => {
+        this.foodEntries = response;
+        console.log(this.foodEntries);
+      })
   }
 }
