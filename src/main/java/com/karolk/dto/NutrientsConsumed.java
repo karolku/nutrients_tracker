8cf87850c6c910b06e59209e291531ca0fe18fd2
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-09-04 00:17:25 +0100

    Created POJO for consumed nutrients

--- src/main/java/com/karolk/dto/NutrientsConsumed.java
+++ src/main/java/com/karolk/dto/NutrientsConsumed.java
@@ -1,4 +1,60 @@
 package com.karolk.dto;
 
 public class NutrientsConsumed {
+    private double calories;
+    private double protein;
+    private double carbohydrated;
+    private double fat;
+
+    public NutrientsConsumed() {
+    }
+
+    public NutrientsConsumed(double calories, double protein, double carbohydrated, double fat) {
+        this.calories = calories;
+        this.protein = protein;
+        this.carbohydrated = carbohydrated;
+        this.fat = fat;
+    }
+
+    public double getCalories() {
+        return calories;
+    }
+
+    public void setCalories(double calories) {
+        this.calories = calories;
+    }
+
+    public double getProtein() {
+        return protein;
+    }
+
+    public void setProtein(double protein) {
+        this.protein = protein;
+    }
+
+    public double getCarbohydrated() {
+        return carbohydrated;
+    }
+
+    public void setCarbohydrated(double carbohydrated) {
+        this.carbohydrated = carbohydrated;
+    }
+
+    public double getFat() {
+        return fat;
+    }
+
+    public void setFat(double fat) {
+        this.fat = fat;
+    }
+
+    @Override
+    public String toString() {
+        return "NutrientsConsumed{" +
+                "calories=" + calories +
+                ", protein=" + protein +
+                ", carbohydrated=" + carbohydrated +
+                ", fat=" + fat +
+                '}';
+    }
 }
