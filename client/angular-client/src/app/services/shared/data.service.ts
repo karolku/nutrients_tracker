47a5810e98fbef57a6c578bcd8a7ca807ec980c5
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-09-04 14:50:16 +0100

    Added fields variables to the shared data service

--- client/angular-client/src/app/services/shared/data.service.ts
+++ client/angular-client/src/app/services/shared/data.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
+
+  private nutrientsConsumed: any;
+  private data = new BehaviorSubject(this.nutrientsConsumed);
+  sharedData = this.data.asObservable();
 
   constructor() { }
 }
