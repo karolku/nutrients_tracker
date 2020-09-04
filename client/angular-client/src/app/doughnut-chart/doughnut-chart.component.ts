6afc066df3f8fb84ab263f3cad72ecfbb7b461d3
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-09-04 15:00:57 +0100

    implmented OnInit interface

--- client/angular-client/src/app/doughnut-chart/doughnut-chart.component.ts
+++ client/angular-client/src/app/doughnut-chart/doughnut-chart.component.ts
@@ -1,15 +1,18 @@
 import { DashboardComponent } from './../dashboard/dashboard.component';
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-doughnut-chart',
   templateUrl: './doughnut-chart.component.html',
   styleUrls: ['./doughnut-chart.component.scss']
 })
-export class DoughnutChartComponent {
+export class DoughnutChartComponent implements OnInit {
 
   nutrientsConsumed: any;
   constructor(private dashboard: DashboardComponent){
+  }
+
+  ngOnInit(){
   }
 
   public chartType: string = 'doughnut';
