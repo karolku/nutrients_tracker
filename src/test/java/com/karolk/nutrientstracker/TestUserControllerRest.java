826dfc0b0a2be5bab39bff2f071906117aa8f0cb
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-09-17 21:47:15 +0200

    Added annotations. @SpringBoottest instead of @RunWith and @ExtendWith

--- src/test/java/com/karolk/nutrientstracker/TestUserControllerRest.java
+++ src/test/java/com/karolk/nutrientstracker/TestUserControllerRest.java
@@ -1,4 +1,17 @@
 package com.karolk.nutrientstracker;
 
+import com.karolk.controller.rest.UserControllerRest;
+import com.karolk.repository.UserRepository;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 public class TestUserControllerRest {
+
+    @InjectMocks
+    UserControllerRest userRest;
+
+    @Mock
+    UserRepository userRepository;
 }
