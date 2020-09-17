770ace980aa5d0f7e9290c8432a0abb8fe52c6aa
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-09-17 21:02:43 +0200

    Added Mockito annotations to the TestFoodsService

--- src/test/java/com/karolk/nutrientstracker/TestFoodsService.java
+++ src/test/java/com/karolk/nutrientstracker/TestFoodsService.java
@@ -1,4 +1,18 @@
 package com.karolk.nutrientstracker;
 
+import com.karolk.repository.FoodsRepository;
+import com.karolk.service.FoodsService;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TestFoodsService {
+
+    @InjectMocks
+    FoodsService foodsService;
+
+    @Mock
+    FoodsRepository foodsRepository;
 }
