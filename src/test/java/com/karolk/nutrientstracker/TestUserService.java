90b5089558c792f7da7a2deb906f8ca0187d0a50
Karol Kustusz <kxk953@cs.bham.ac.uk>
2020-09-17 19:32:20 +0200

    Added RunWith and InjectMocks annotations

--- src/test/java/com/karolk/nutrientstracker/TestUserService.java
+++ src/test/java/com/karolk/nutrientstracker/TestUserService.java
@@ -1,4 +1,18 @@
 package com.karolk.nutrientstracker;
 
+import com.karolk.repository.UserRepository;
+import com.karolk.service.UserService;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TestUserService {
+
+    @InjectMocks
+    UserService userService;
+
+    @Mock
+    UserRepository userRepository;
 }
